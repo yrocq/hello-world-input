47094976b419f227ee0203105849a10af3143548
Yann Rocq <yann.rocq@pinq2.com>
2026-09-15 14:07:51 -0400

    Transform input

--- hello.sh
+++ hello.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 mkdir -p /tmp/outputs /tmp/logs
 chmod 755 /tmp/outputs /tmp/logs
-env
-echo Hello world
-echo This is an output test > /tmp/outputs/test.txt
+
+cp /tmp/input-files/input.txt /tmp/outputs/test.txt
+
+echo New line added >> /tmp/outputs/test.txt
+
+echo Input content:
+cat /tmp/input-files/test.txt
+
 echo Output content:
 cat /tmp/outputs/test.txt
-cat /tmp/input-files/input.txt
+
